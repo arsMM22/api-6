b1737b6fd220b4dc910926460f11357d7cee70ff
arsMM22 <129196828+arsMM22@users.noreply.github.com>
2024-10-05 15:33:25 +0300

    удалил временные файлы в случае ошибки

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ def get_parametr_comics(number):
     url = f"https://xkcd.com/{number}/info.0.json"
     response = requests.get(url)
     response.raise_for_status()
-    alt = response.json()["alt"]
-    image = response.json()["img"]
-    name = response.json()["title"]
+    comic = response.json()
+    alt = comic["alt"]
+    image = comic["img"]
+    name = comic["title"]
     return alt, image, name
 
 
@@ -41,13 +42,15 @@ def main():
     load_dotenv()
     tg_chat_id = os.environ['TG_CHAT_ID']
     tg_token = os.environ['TELEGRAM_TOKEN']
-    all_numbers = get_num_comics()
-    random_number = random.randint(1, all_numbers)
-    alt, image, name = get_parametr_comics(random_number)
-    filename = f"{name}.png"
-    download_comics(image, filename)
-    upload_comics(tg_token, filename, tg_chat_id, alt)
-    os.remove(filename)
+    try:
+        all_numbers = get_num_comics()
+        random_number = random.randint(1, all_numbers)
+        alt, image, name = get_parametr_comics(random_number)
+        filename = f"{name}.png"
+        download_comics(image, filename)
+        upload_comics(tg_token, filename, tg_chat_id, alt)
+    finally:
+        os.remove(filename)
 
 
 if __name__ == "__main__":
